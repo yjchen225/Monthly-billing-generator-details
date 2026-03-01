a437e2731e51f450c80213868cad469f4a0cba61
yjchen225 <yjchen225@gmail.com>
2026-03-01 18:21:19 +0800

    fix wrong base path

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Monthly-billing-generator-details/", // ← 改成你的 repo 名稱（大小寫要一致）
   plugins: [react()],
 });
